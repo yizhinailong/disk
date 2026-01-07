c50ab7b2146a2b2af010d16bc793d1844adeded8
yizhinailong <liufeng.code@outlook.com>
2026-01-08 01:06:42 +0800

    refactor(build): remove PCH setup script and use direct target configuration
    - Remove cmake/setup_pch.cmake file that contained precompiled header setup function
    - Remove include of setup_pch.cmake from main CMakeLists.txt
    - Configure precompiled headers directly in src/CMakeLists.txt using target_precompile_headers
    - Point to pch.hpp header file in src/utils/pch.hpp instead of function call

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
-#include <print>
-
-#include <drogon/drogon.h>
-
-using Callback = std::function<void(const drogon::HttpResponsePtr&)>;
+#include "utils/pch.hpp"
 
 int main() {
     drogon::app().registerHandler(
         "/hello",
-        [](const drogon::HttpRequestPtr& req, Callback&& callback) {
+        [](const drogon::HttpRequestPtr& req, HttpResponseCallback&& callback) {
             auto resp = drogon::HttpResponse::newHttpResponse();
             resp->setBody("Hello World");
             std::move(callback)(resp);
@@ -16,7 +12,6 @@ int main() {
     std::println("Drogon version: {}", drogon::getVersion());
     std::println("Web server is listening on http://127.0.0.1:8080");
     std::println("The index page is http://127.0.0.1:8080/hello");
-    std::println("Drogon is running...");
 
     drogon::app().loadConfigFile("config.json").run();
 
---
+++ src/utils/pch.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file pch.hpp
+ * @brief 预编译头文件，包含常用的头文件和类型别名
+ * @version 0.1
+ * @date 2026-01-08
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+// Drogon 框架头文件
+#include <drogon/HttpAppFramework.h>
+#include <drogon/HttpClient.h>
+#include <drogon/HttpController.h>
+#include <drogon/HttpRequest.h>
+#include <drogon/HttpResponse.h>
+#include <drogon/drogon.h>
+
+// C++ 标准库常用头文件
+#include <algorithm>
+#include <chrono>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <print>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+// 其他常用头文件
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+
+// 项目通用类型别名
+using HttpResponseCallback = std::function<void(const drogon::HttpResponsePtr&)>;
